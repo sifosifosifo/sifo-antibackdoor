f42bec6769ecd5537eb470ab1237b900ad472783
SIFO <124735333+sifosifosifo@users.noreply.github.com>
2026-09-25 01:13:10 +0100

    Refactor scanner into modular server files

---
+++ server/commands.lua
@@ -0,0 +1,54 @@
+function SIFO.startScan()
+    if SIFO.ScanRunning then
+        print("^3[SIFO] A scan is already running.^7")
+        return
+    end
+
+    SIFO.ScanRunning = true
+    SIFO.reset()
+
+    print("^5[SIFO] Threat intelligence scan started...^7")
+    SIFO.scanAllResources()
+    SIFO.writeReport()
+    SIFO.sendDiscordReport()
+    SIFO.printSummary()
+    SIFO.ScanRunning = false
+end
+
+RegisterCommand("sifo_scan", function(source)
+    if source ~= 0 then return end
+    SIFO.startScan()
+end, false)
+
+RegisterCommand("sifo_risk", function(source)
+    if source ~= 0 then return end
+    SIFO.printResourceRisk()
+end, false)
+
+AddEventHandler("onResourceStart", function(resource)
+    if resource == SIFO.ResourceName then
+        if not Config.Scanner.ScanOnResourceStart then return end
+
+        CreateThread(function()
+            Wait(Config.Scanner.ScanDelay)
+            SIFO.startScan()
+        end)
+
+        return
+    end
+
+    CreateThread(function()
+        Wait(750)
+        if GetResourceState(resource) == "started" then
+            SIFO.scanResource(resource)
+        end
+    end)
+end)
+
+if not SIFO_THREATS then
+    print("^1[SIFO] Threat database failed to load.^7")
+end
+
+if not SIFO_COMBINATION_RULES then
+    print("^1[SIFO] Combination rules failed to load.^7")
+end
